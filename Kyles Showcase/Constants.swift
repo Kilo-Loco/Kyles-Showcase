52f378abaf30004101bc54d4d88d09e37406a6bb
Kyle Lee <kiloloco@me.com>
2015-11-19 15:56:30 -0800

    Finished login page layout and launch screen view

---
+++ Kyles Showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Kyles Showcase
+//
+//  Created by Kyle Lee on 11/19/15.
+//  Copyright © 2015 Kyle Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 167.0 / 255.0
